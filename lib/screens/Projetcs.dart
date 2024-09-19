61a41c65991b5641418cbd58ac91ca0f74b0eff5
varun <mulukanoorvarun2001@gmail.com>
2024-09-19 19:23:53 +0530

    committed by varun at office 7:23 pm 19-09-2024

---
+++ lib/screens/Projetcs.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProjectsScreen extends StatefulWidget {
+  const ProjectsScreen({super.key});
+
+  @override
+  State<ProjectsScreen> createState() => _ProjectsScreenState();
+}
+
+class _ProjectsScreenState extends State<ProjectsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
