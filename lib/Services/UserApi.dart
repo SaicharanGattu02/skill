4dabae4d5656440b27c386e44de1ba483b441930
varun <mulukanoorvarun2001@gmail.com>
2024-09-23 13:54:55 +0530

    committed by varun at office 1:54 pm 23-09-2024

--- lib/Services/UserApi.dart
+++ lib/Services/UserApi.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
-import '../Models/RegisterModel.dart';
+
+import '../Model/LoginModel.dart';
+import '../Model/RegisterModel.dart';
 
 
 class Userapi {
-
-
 
   static String host="http://192.168.0.56:5000";
 
@@ -34,5 +34,42 @@ class Userapi {
       return null;
     }
   }
+
+  static Future<LoginModel?> PostLogin(String mail, String password) async {
+    try {
+      Map<String, String> data = {
+
+        "email": mail,
+        "password": password,
+
+      };
+      final url = Uri.parse("http://192.168.0.56:5000/auth/login");
+      final response = await http.post(
+        url,
+        headers: {
+          HttpHeaders.contentTypeHeader: "application/json",
+        },
+        body: jsonEncode(data),
+      );
+      if (response!=null) {
+        final jsonResponse = jsonDecode(response.body);
+        print("PostRegister Status:${response.body}");
+        return LoginModel.fromJson(jsonResponse);
+      } else {
+        print("Request failed with status: ${response.statusCode}");
+        return null;
+      }
+    } catch (e) {
+      print("Error occurred: $e");
+      return null;
+    }
+  }
+
+
+
+
+
+
+
 
 }
