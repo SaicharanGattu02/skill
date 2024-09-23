4cfa4961ca8bae9f13d8f96b49dfa68487e172a0
MYPC <Charangattu643@gmail.com>
2024-09-23 11:37:50 +0530

    Commited By Charan 23-09-2024 At 11:37 PM

---
+++ lib/utils/constants.dart
@@ -0,0 +1,50 @@
+const defaultPadding = 10.0;
+const defaultMargin = 10.0;
+const defaultHeight = 10.0;
+const defaultWidth = 10.0;
+const defaultButtonHeight = 50.0;
+const defaultRadius = 10.0;
+const connectionTimeOut = 60;
+
+double LargeTextSize = 18;
+double TextheaderSize = 16;
+double TextlabelSize = 12;
+double textSize = 12;
+double subtextSize = 10;
+double minisubtextSize = 8.0;
+double XLargeTextSize = 24;
+
+// // var PhonePeMerchantId="NUTSBYONLINE";//Production Key
+var PhonePeMerchantId="";//PGTESTPAYUAT
+// var PhonePeSaltKey="70508497-24ec-4372-bb9f-f7ea4b451f7d";//Production Key
+var PhonePeSaltKey="";  //099eb0cd-02cf-4e2a-8aca-3e6c6aff0399
+var PhonePeSaltIndex="1";
+var AppId="ddeca623b5d548a486c0fcd062479880";//cce1254412334734990aa88f6a382b4a
+
+// var PhonePeMerchantId="SVINDOONLINE";//production
+// var PhonePeSaltKey="78a4f850-4cb2-467d-a5ca-fd8f04420518";//production
+// var PhonePeSaltIndex="1";
+// var AppId="in.webgrid.svindo";
+
+double screenSpace=10;
+
+const fontFamilyName = "Ubuntu";
+const dreamBikeFont = "nasalization";
+const authorization = "Authorization";
+const imageQuality = 50;
+const dateFormat = "yy/MM/dd";
+const timeFormat = "HH:mm:ss";
+const sliderHeight = 180.0;
+const rupeeSymbol = "₹";
+const fav_icon="assets/images/saveicon.svg";
+const mapsApiKey = "AIzaSyCA06NWEP5D-z8WpebENgd4mSOqV-uXIUE";
+const upiRegex = "[a-zA-Z0-9.-]{2,256}@[a-zA-Z][a-zA-Z]{2,64}";
+const vehicleNumberRegex =
+    "^[a-zA-Z]{2}[0-9]{1,2}(?:[a-zA-Z])?(?:[a-zA-Z]*)?[0-9]{4}";
+const panNumberRegex = "[A-Z]{5}[0-9]{4}[A-Z]{1}";
+const aadhaarRegex = "[0-9]{12}";
+
+const staticImage = "https://picsum.photos/250?image=9";
+
+
+var notification_val=0;
